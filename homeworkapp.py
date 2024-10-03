c89a0a3c052851fabd39fa013d8dbba4301af26b
Ryushe <ryushe.dev@gmail.com>
2024-10-03 16:51:14 -0700

    handling cookies better, and major changes

--- homeworkapp.py
+++ homeworkapp.py
@@ -25,42 +25,65 @@ class HomeworkApp:
         self.frm.grid(padx=10, pady=10)
 
         self.url_var = StringVar()
-        self.hw = None
+        self.driver = None
         self.cookie_file = "cookies.pkl"
 
         self.setup_ui()
+    
+    def show_current_url(self):
+        if self.driver is not None:
+            current_url = self.driver.current_url  # No parentheses needed
+            # Update the label with the current URL
+            self.url_label.config(text=f"Current Url: {current_url}")  
+    
 
     def setup_ui(self):
         main_pady=3
         main_padx=3
+        if self.driver != None:
+            current_url = self.driver.current_url
+        #row 0
         Label(self.frm, text="Enter HW URL:").grid(column=0, row=0, sticky=W, padx=main_padx, pady=main_pady)
-
         Entry(self.frm, textvariable=self.url_var).grid(column=1, row=0, sticky=W, padx=main_padx, pady=main_pady)
         
         #row 1
         Button(self.frm, text="Set URL", command=self.set_url, width=15).grid(column=0, row=1, sticky=W, padx=main_padx, pady=main_pady)
         Button(self.frm, text="Go to Canvas", command=self.set_canvas_url, width=15).grid(column=1, row=1, sticky=W, padx=main_padx, pady=main_pady)
-        Button(self.frm, text="Save Cookies", command=self.save_cookies, width=15).grid(column=2, row=1, sticky=W, padx=main_padx, pady=main_pady)
-        Button(self.frm, text="Load Cookies", command=self.load_cookies, width=15).grid(column=3, row=1, sticky=W, padx=main_padx, pady=main_pady)
+        Button(self.frm, text="Load Canvas Cookies", command=self.load_cookies, width=15).grid(column=2, row=1, sticky=W, padx=main_padx, pady=main_pady)
+        Button(self.frm, text="Save Cookies", command=self.save_cookies, width=15).grid(column=3, row=1, sticky=W, padx=main_padx, pady=main_pady)
         #row 2
         Button(self.frm, text="Start", command=self.get_questions, width=15).grid(column=0, row=2, sticky=W, padx=main_padx, pady=main_pady)
         Button(self.frm, text="Pause", command=self.root.destroy, width=15).grid(column=1, row=2, sticky=W, padx=main_padx, pady=main_pady)
         Button(self.frm, text="Quit", command=self.root.destroy, width=15).grid(column=2, row=2, sticky=W, padx=main_padx, pady=main_pady)
+    
+    def remove_needs_cookie(self):
+        current_url = self.driver.current_url()
+        cookie_url = "login?needs_cookies=1"
+        if cookie_url in current_url: 
+            new_url = current_url.replace(cookie_url, "")
+            self.set_url(new_url)
+            log.info("Adjusted canvas url to login")
 
     def save_cookies(self):
         try:
             with open(self.cookie_file, 'wb') as file:
-                pickle.dump(self.hw.get_cookies(), file)
+                pickle.dump(self.driver.get_cookies(), file)
+            file.close()
         except Exception as e:
             log.error(e)
 
     def load_cookies(self):
+        url = self.url_var.get().strip()
+        self.set_url(url)
+        # self.remove_needs_cookie()
+        
         try:
             with open(self.cookie_file, 'rb') as file:
                 cookies = pickle.load(file)
                 for cookie in cookies:
-                    self.hw.add_cookie(cookie)
+                    self.driver.add_cookie(cookie)
             log.info("Cookies Succesfully loaded")
+            file.close()
         except FileNotFoundError:
             log.info("No cookies to load")
         except Exception as e:
@@ -68,19 +91,19 @@ class HomeworkApp:
     
 
     def set_url(self, url=""):
-        if self.hw is None:
-            self.hw = Site("test")  # Assuming Site is a class that initializes the driver
+        if self.driver is None:
+            self.driver = Site("profile")  # Assuming Site is a class that initializes the driver
         try:
             url = self.url_var.get().strip()
-            self.hw.get(url)
+            self.driver.get(url)
             self.url_var.set("")  # Clear the input field
-        except AttributeError:
+        except AttributeError or ValueError:
             log.info("no url in box")
         except Exception as e:
             log.error(f"{e}")
 
     def set_canvas_url(self):
-        canvas_url = "https://ctclink.okta.com/app/ctclink_canvaswenatcheevalleycollege_1/exk8ykhtdcsBEjxNz4x7/sso/saml?SAMLRequest=fVLLbsIwELz3KyLfkzhpgNYCJCititQHKrSHXpBxFnBx7NTr8OjX14RWpQe4zs7s7Iy2jbxQJetVbqlf4LMCdMG2UBpZPeiQympmOEpkmheAzAk27j0%2BsDSirLTGGWEUOZKcV3BEsE4aTYLhoEOmzVZGm8k1DbMrnodZzmk4oyIPG0mzdS2ajUQAJ8EbWPSaDvErvBCxgqFGx7XzEE2zMKEhTSc0ZZeUNRrvJBj4HFJzV6uWzpXI4lg4oaReRWbleCRMEfOy%2FAWngus1xw14kVgCrLlSsPPZFCxgmsSwXV3tVkuXC%2BzffmyfvrJtK0Y08T4zCXq%2FuW6MxqoAOwa7lgJeXx7%2B%2FDdrEUl%2Ft62EqyzUJyizkPpnyeinzr7UudSL803ODiRk95PJKBw9jyek297vYXU%2Ftrt3PWG6p6Xt%2BJjdPvzBk%2FcZDkZGSbEL7owtuDt9RhIlNSLzcF5TWaWxBCHnEnLfiVJmc2OBO%2BgQ7w8k7h5M%2F%2F9b9%2BIb"
+        canvas_url = "https://wvc.instructure.com/login?needs_cookies=1"
         self.set_url(canvas_url)
     
     def query_chatgpt(self):
@@ -91,7 +114,7 @@ class HomeworkApp:
     
     def do_checkboxes(self): # Place holder code
         xpath_for_span = "//span[contains(@class, 'rvTxt') and normalize-space(text()) != '']"
-        spans = self.hw.find_elements(By.XPATH, xpath_for_span)
+        spans = self.driver.find_elements(By.XPATH, xpath_for_span)
 
         for span in spans:
             text_content = span.text.strip()
@@ -112,31 +135,45 @@ class HomeworkApp:
                 print(f"Could not find or check the checkbox for: {text_content} - {e}")
     def check_question_type(self):
         print('lol')
+
+    def go_question_iframe(self):
+        try:
+            iframe_xpath = "//iframe[contains(@title, 'Question Viewer')]"
+            iframe_id = "contentFrame"
+            iframe_tag = "iframe"
+            self.driver.wait_for_all(By.TAG_NAME, iframe_tag)
+            question_iframe = self.driver.find_element(By.TAG_NAME, iframe_tag)
+            self.driver.switch_to.frame(question_iframe)
+            self.driver.implicitly_wait(2)
+            log.info("Sucessfully moved into iframe")
+        except Exception as e:
+            log.error(e)
     
     def get_questions(self):
-        # xpath_for_span = "//span[contains(@class, 'rvTxt') and normalize-space(text()) != '']"
         #containers
+        self.go_question_iframe()
         top_question_container_xpath = "//div[contains(@id, 'top') and contains(@class, 'dijitContentPane')]"
         mc_questions_xpath = "//span[@class='step'][.//div[@data-dojo-type='xl.player.controls.MultipleChoiceAnswer']]" # gets only questions and the mc choieces
         try:
-            self.top_question = self.hw.find_element(By.XPATH, top_question_container_xpath)
-            self.mc_questions = self.hw.find_elements(By.XPATH, mc_questions_xpath)
+            self.top_question = self.driver.find_element(By.XPATH, top_question_container_xpath)
+            self.mc_questions = self.driver.find_elements(By.XPATH, mc_questions_xpath)
             log.info(f"""Top Level Question:
 {self.top_question.text.strip()}                
 Mc Questions:
 {'\n'.join([question.text.strip() for question in self.mc_questions])}
 """)
+# NEXT TO DO, MAKE SURE THIS WORKS X SEE IF I CAN MAKE SSO NOT REDIRECT OR HOW I CAN SSO WITH SELENIUM
         except AttributeError:
             log.info("No questions found (supported: mc)")
         except Exception as e:
             log.error(f"An error has occured {e}")
         
     def create_driver(self, url=''):
-        if self.hw is None:
-            self.hw = Site("test")  # Assuming Site is a class that initializes the driver
+        if self.driver is None:
+            self.driver = Site("test")  # Assuming Site is a class that initializes the driver
             # self.login()  # Login after initializing the driver
         if url:
-            self.hw.set_url(url)  # Assuming set_url is a method of the Site class
+            self.driver.set_url(url)  # Assuming set_url is a method of the Site class
 
 
 if __name__ == "__main__":
@@ -154,29 +191,4 @@ if __name__ == "__main__":
 #     console.log(textContent.trim()); // Print the text content without leading/trailing whitespace
 # }
 
-
-# possible python for checkboxes
-
-# xpath_for_span = "//span[contains(@class, 'rvTxt') and normalize-space(text()) != '']"
-# spans = driver.find_elements(By.XPATH, xpath_for_span)
-
-# for span in spans:
-#     text_content = span.text.strip()
-#     print(f"Found span with text: {text_content}")
-
-#     # Example logic: find the checkbox associated with the span
-#     # Assuming the checkbox is located near the span (adjust XPath as necessary)
-#     try:
-#         checkbox_xpath = ".//following::input[@type='checkbox'][1]"  # Finds the first checkbox following the span
-#         checkbox = span.find_element(By.XPATH, checkbox_xpath)
-        
-#         # Check the checkbox if it's not already checked
-#         if not checkbox.is_selected():
-#             checkbox.click()
-#             print(f"Checked the checkbox related to: {text_content}")
-#         else:
-#             print(f"Checkbox already checked for: {text_content}")
-    
-#     except Exception as e:
-#         print(f"Could not find or check the checkbox for: {text_content} - {e}")
 
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import itertools
+import platform
 import sys
 import re
 import os
@@ -9,107 +10,41 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-# not using
-class Driver:
-  def __init__(self):
-    self.options = Options()
-    self.options = webdriver.ChromeOptions() 
-    self.options.add_argument("--log-level=3")
-    self.options.add_argument('--headless')
-    self.driver = webdriver.Chrome(options=self.options)
-  def search(self, url):
-    return self.driver.get(url)
-  def wait(self, time):
-    return WebDriverWait(self.driver, time)
-
-
-def test_driver(url, time=10):
-  driver = webdriver.Chrome()
-  driver.get(url)
-  wait = WebDriverWait(driver, time)
-  return driver, wait
-    
-def make_driver(url, time=10):
-  options = Options()
-  options = webdriver.ChromeOptions() 
-  options.add_argument("--log-level=3")
-  options.add_argument('--headless')
-  driver = webdriver.Chrome(options=options)
-  driver.get(url)
-  wait = WebDriverWait(driver, time)
-
-  return driver, wait
-  
-
-def is_similar(str1, str2, max_diff=2):
-  if abs(len(str1) - len(str2)) > max_diff:
-    return False
-  diff_count = 0
-  for i in range(min(len(str1), len(str2))):
-    if str1[i] != str2[i]:
-      diff_count += 1
-      if diff_count > max_diff:
-        return False
-  return True
-
-def non_specialify(word):
-  pattern = r"[^\w\s]"
-  non_special_word = re.sub(pattern, '', word)
-  return non_special_word.replace(',', '')
-
-
-def dearray(nested_list):
-  return list(itertools.chain.from_iterable(nested_list))
-
-def is_decimal(number: str):
-  if isinstance(number, str) and '.' in number:
-    return True
-  return False
-
-def has_decimal(numbers):
-  for num in numbers:
-    if isinstance(num, float):
-      return True
-  return False  
-
-def get_folder_files(path):
-    folder_files = []
-    for file in os.listdir(path):
-        file_path = os.path.join(path, file)
-        if os.path.isfile(file_path):
-            folder_files.append(file_path)
-    return folder_files
-     
-
-
-def cammel_case(words):
-  split_words = words.split()
-  capital = [word.capitalize() for word in split_words]
-  capital_words = ' '.join(capital)
-  return capital_words
-  
 
 class Site(webdriver.Chrome):
   def __init__(self, driver=None):
       # Initialize options once
-      options = Options()
-      options.add_argument("--log-level=3")
-      options.add_argument("--disable-popup-blocking")  # Prevents new tabs from opening
+      self.options = webdriver.ChromeOptions()
+      self.options.add_argument("--log-level=3")
+      self.options.add_argument("--disable-popup-blocking")  # Prevents new tabs from opening
 
-      if driver and isinstance(driver, webdriver.Chrome):
-          self.driver = driver
-      elif driver == "test":
-          super().__init__(options=options)  
-          self.get("https://ctclink.okta.com/app/ctclink_canvaswenatcheevalleycollege_1/exk8ykhtdcsBEjxNz4x7/sso/saml?SAMLRequest=fVLLbsIwELz3KyLfkzhpgNYCJCititQHKrSHXpBxFnBx7NTr8OjX14RWpQe4zs7s7Iy2jbxQJetVbqlf4LMCdMG2UBpZPeiQympmOEpkmheAzAk27j0%2BsDSirLTGGWEUOZKcV3BEsE4aTYLhoEOmzVZGm8k1DbMrnodZzmk4oyIPG0mzdS2ajUQAJ8EbWPSaDvErvBCxgqFGx7XzEE2zMKEhTSc0ZZeUNRrvJBj4HFJzV6uWzpXI4lg4oaReRWbleCRMEfOy%2FAWngus1xw14kVgCrLlSsPPZFCxgmsSwXV3tVkuXC%2BzffmyfvrJtK0Y08T4zCXq%2FuW6MxqoAOwa7lgJeXx7%2B%2FDdrEUl%2Ft62EqyzUJyizkPpnyeinzr7UudSL803ODiRk95PJKBw9jyek297vYXU%2Ftrt3PWG6p6Xt%2BJjdPvzBk%2FcZDkZGSbEL7owtuDt9RhIlNSLzcF5TWaWxBCHnEnLfiVJmc2OBO%2BgQ7w8k7h5M%2F%2F9b9%2BIb")
+      if driver == "test":
+          super().__init__(options=self.options)  
+      elif driver == "profile":
+          self.setup_profile()
+          super().__init__(options=self.options)  
+          self.get("https://wvc.instructure.com/login?needs_cookies=1")
       elif driver == "remote":
-          options.add_experimental_option("debuggerAddress", "127.0.0.1:6080")
-          super().__init__(options=options)  # Initialize the base class
+          self.options.add_experimental_option("debuggerAddress", "127.0.0.1:6080")
+          super().__init__(options=self.options)  # Initialize the base class
       else:
-          super().__init__(options=options)  
-          options.add_argument('--headless')  
+          super().__init__(options=self.options)  
+          self.options.add_argument('--headless')  
+
   def set_url(self, url):
       self.driver.get(url)
       print(self.driver.title)
+  
+  def setup_profile(self, profile=os.path.join(".", "profiles", "main")): #could make more than one profile
+      if not os.path.exists(profile):
+          print(f"Profile not found, creating it at {profile}")
+          os.makedirs(profile)  # Create the profile directory
+
+      # Add profile-related options
+      self.options.add_argument(f"--user-data-dir={profile}")
+      self.options.add_argument("--no-first-run")
+      self.options.add_argument("--no-default-browser-check")
+      
 
   def button_search(self, searchbar_id, button_id, search_query, method=By.ID):
     search = self.driver.find_element(method, searchbar_id)
